5df659598f5a56cc716ff162c56e3351d1dd0c46
Patrichuan <Patrichuan@gmail.com>
2015-01-15 17:11:09 +0100

    Update Answer.java

--- src/Answer.java
+++ src/Answer.java
@@ -1,36 +1,39 @@
 import java.io.Serializable;
 
 public class Answer implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	private int id_question;
 	private String text;
 	private boolean is_correct;
 
-	public Answer() {
-		text = "";
-		is_correct = false;
-	}
-
-	public Answer(String text, boolean is_correct) {
-		super();
+	public Answer(int id_question, String text, boolean is_correct) {
+		this.id_question = id_question;
 		this.text = text;
 		this.is_correct = is_correct;
 	}
 
-	public String setText(String text) {
-		return this.text = text;
+	public void setText(String text) {
+		this.text = text;
 	}
 
 	public String getText() {
 		return text;
 	}
-
-	public boolean setIs_correct(boolean is_correct) {
-		return this.is_correct = is_correct;
-
+	
+	public void setIdQuestion (int id_question) {
+		this.id_question = id_question;
+	}
+	
+	public int getIdQuestion () {
+		return id_question;
+	}
+	
+	public void set_isCorrect(boolean is_correct) {
+		this.is_correct = is_correct;
 	}
 
 	public boolean isCorrect() {
 		return is_correct;
-
 	}
-
 }
