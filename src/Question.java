98558da98fe8555d6e8f85123377e89bd14d6df3
Patrichuan <Patrichuan@gmail.com>
2015-01-15 17:11:31 +0100

    Update Question.java

--- src/Question.java
+++ src/Question.java
@@ -1,25 +1,30 @@
 import java.io.Serializable;
 
 public class Question implements Serializable {
-    private String text;
-    private int id;
+	
+	private static final long serialVersionUID = 1L;
+	private String text;
+	private int id;
 
-    public Question() {
-	text = "";
-	id = 0;
-    }
+	public Question(int id, String text) {
+		this.id = id;
+		this.text = text;		
+	}
+	
+	public void setText(String text) {
+		this.text = text;
+	}
 
-    public Question(String text, int id) {
-	this.text = text;
-	this.id = id;
-    }
+	public String getText() {
+		return text;
+	}
 
-    public String getText() {
-	return text;
-    }
-
-    public void setText(String text) {
-	this.text = text;
-    }
-
+	public  void setId(int id) {
+		this.id = id;
+	}
+	
+	public int getId() {
+		return id;
+	}
+	
 }
